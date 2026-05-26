29840efc7c8f3e17881a1cf68f3fc25cac2bf50e
Andrii Prokhorenko <andriiprokhorenko@Andriis-MacBook-Pro.local>
2026-05-26 14:55:42 -0500

    Multi account feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { TelegramClient, Api } from "telegram";
 import { StringSession } from "telegram/sessions/StringSession.js";
-import { input, password } from "@inquirer/prompts";
+import { input, password, select } from "@inquirer/prompts";
 import { ExitPromptError } from "@inquirer/core";
 import keytar from "keytar";
 import { match, none, run, some } from "./option.js";
@@ -8,8 +8,6 @@ import { type Option } from "./option.js";
 
 // later will be moved to real .env
 const storageName = "telega";
-const tokenKey = "session_token";
-
 
 type EnvVariables = {
     apiId: number,
@@ -18,9 +16,60 @@ type EnvVariables = {
 
 async function main() {
     const env = await getEnvVariables()
-    const tokenOrNone = await getSavedToken()
-    const client = createTelegramClient(tokenOrNone, env) 
 
+    const accounts = await getAccounts()
+
+    const quitAppValue = "quit_app"
+
+    const quitAppItem = ({
+        name: "Quit",
+        value: quitAppValue,
+    }) 
+
+    const addAccountValue = "add_new_account"
+
+    const addAccountItem = ({
+        name: "Add new account",
+        value: addAccountValue,
+    })
+
+    const accountItems = accounts.keys()
+        .map(item => ({ name: item, value: item }))
+
+    const choices = [quitAppItem, addAccountItem, ...accountItems]
+
+    let choice: string
+
+    try {
+        choice = await select({ 
+            message: "Select:", 
+            choices: choices,
+            theme: {
+                keybindings: [ "vim" ]
+            },
+        }) as string 
+    } catch (error) {
+        if (error instanceof ExitPromptError) {
+            process.exit(0)
+        }
+        throw error
+    }
+
+    let tokenOrNone: Option<string> 
+    if (choice === quitAppValue) {
+        process.exit(0) 
+    } else if (choice === addAccountValue) {
+        tokenOrNone = none()
+    } else {
+        const choiceToken = accounts.get(choice)
+        if (choiceToken === undefined) {
+            tokenOrNone = none()
+        } else {
+            tokenOrNone = some(choiceToken)
+        }
+    }
+
+    const client = createTelegramClient(tokenOrNone, env)
     run(
         tokenOrNone, 
         {
@@ -32,7 +81,7 @@ async function main() {
             ifSome: async () => {
                 // if signed in
 
-                await signedUserFlow(client)
+                await signedUserFlow(client, choice)
             },
         }
     )
@@ -73,31 +122,31 @@ async function getPhoneCode(): Promise<string> {
     }
 }
 
-async function getSavedToken(): Promise<Option<string>> {
-    const token = await keytar.getPassword(storageName, tokenKey)
-    if (token == null) {
-        return none()
-    }
-
-    return some(token)
+async function getAccounts(): Promise<Map<string, string>> {
+    const accounts = await keytar.findCredentials(storageName)
+    const accountMap = new Map<string, string>()
+    accounts.forEach(account => {
+        accountMap.set(account.account, account.password)
+    })
+    return accountMap
 }
 
-async function signedUserFlow(client: TelegramClient): Promise<void> {
+async function signedUserFlow(client: TelegramClient, userId: string): Promise<void> {
     console.log("logged")
 
     await new Promise(resolve => setTimeout(resolve, 4 * 1000));
 
-    await signOut(client)
-    await anonUserFlow(client)
+    // await signOut(client, userId)
+    // await anonUserFlow(client)
 }
 
 async function anonUserFlow(client: TelegramClient): Promise<void> {
     console.log("anon")
-    await signIn(client)
-    await signedUserFlow(client)
+    const userId = await signIn(client)
+    await signedUserFlow(client, userId)
 }
 
-async function signIn(client: TelegramClient) {
+async function signIn(client: TelegramClient): Promise<string> {
     await client.start({
         phoneNumber: getPhoneNumber,
         password: getPassword,
@@ -106,13 +155,19 @@ async function signIn(client: TelegramClient) {
     }) 
 
     const newToken = (client.session as StringSession).save()
-    await keytar.setPassword(storageName, tokenKey, newToken)
+    const me = await client.getMe()
+
+    const userId = me.id
+    const userIdStr = `${userId}`
+    await keytar.setPassword(storageName, userIdStr, newToken)
+
+    return userIdStr
 }
 
-async function signOut(client: TelegramClient): Promise<void> {
+async function signOut(client: TelegramClient, userId: string): Promise<void> {
     await Promise.all([
         client.invoke(new Api.auth.LogOut()),
-        keytar.deletePassword(storageName, tokenKey)
+        keytar.deletePassword(storageName, userId)
     ])
 }
 
